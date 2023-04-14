628960d15ae562228f05a7d59214d56b4d17781f
Martin Pizango <eduardo.martin688@gmail.com>
2023-04-14 09:25:23 -0500

    eliminando código comentado

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,9 +27,6 @@ export default {
       screens: {
         xs: '450px'
       },
-      // backgroundImage: {
-      //   "gradient-hero": "linear-gradient(to bottom, rgba(21, 20, 27, 1), rgba(21, 20, 27, 0.8), rgba(21, 20, 27, 0.5), rgba(21, 20, 27, 0.7), rgba(21, 20, 27, 0.97), rgba(21, 20, 27, 1)), url('https://images.pexels.com/photos/2653362/pexels-photo-2653362.jpeg')",
-      // },
       backgroundImage: {
         "gradient-hero-dark": "linear-gradient(to bottom, rgba(21, 20, 27, 1), rgba(21, 20, 27, 0.8), rgba(21, 20, 27, 0.5), rgba(21, 20, 27, 0.7), rgba(21, 20, 27, 0.97), rgba(21, 20, 27, 1)), url('/src/assets/bg-image-dark.webp')",
         "gradient-hero-light": "linear-gradient(to bottom, rgba(255, 255, 255, 0.25), rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.05), rgba(255, 255, 255, 0.15), rgba(255, 255, 255, 0.5)), url('/src/assets/bg-image-light.webp')",
